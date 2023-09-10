7548ff63f855296899ade3fd64d99cfec31094fb
Mateusz Szymaniak <maszipl@gmail.com>
2023-09-10 14:31:57 +0200

    feat(fridge): add blank fridge app

--- food_helper/settings.py
+++ food_helper/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UserConfig",
+    "fridges.apps.FridgesConfig",
     "crispy_forms",
     "crispy_bootstrap4",
     "django.contrib.admin",
