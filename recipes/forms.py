4dda6b7101cae22c6f1f094e8a639a0f0201cfe9
Mateusz Szymaniak <maszipl@gmail.com>
2023-09-12 19:15:07 +0200

    fix(recepie_add): fix sending ingredients to db

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,8 +1,9 @@
 import json
 
 from django import forms
-from .models import Recipe
 from django.forms import formset_factory
+
+from .models import Recipe
 
 
 class IngredientsForm(forms.Form):
@@ -27,20 +28,15 @@ class RecipeIngredientsForm(forms.Form):
         recipe_name = self.data.get("recipe_name")
         preparation = self.data.get("preparation")
         tags = self.data.get("tags")
-        ingredients_amount = len(
-            list(filter(lambda key: key.startswith("quantity-"), self.data.keys()))
+        ingredients_list = list(
+            filter(lambda key: key.startswith("quantity-"), self.data.keys())
         )
         ingredients_json = {}
-        for prefix in range(ingredients_amount):
-            quantity = self.data.get(f"quantity-{prefix}")
-            name = self.data.get(f"name-{prefix}")
-            if quantity and name:
-                ingredients_json[name] = quantity
-        # for var in list(self.data.keys()):
-        #     if "quantity-" in var:
-        #         ingredients_json = {'quantity': self.data.get(var)}
-        #     elif "name-" in var:
-        #         ingredients_json = {"name": self.data.get(var)}
+        for prefix in ingredients_list:
+            quantity = self.data.get(prefix)
+            name = self.data.get(f"{prefix.replace('quantity', 'name')}")
+
+            ingredients_json[name] = quantity
 
         recipe = Recipe(
             recipe_name=recipe_name,
