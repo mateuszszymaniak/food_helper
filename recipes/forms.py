0b3a0cbb4fc24bbdcb8e7617d55b8c357971dc4e
Mateusz Szymaniak <maszipl@gmail.com>
2023-09-15 21:55:23 +0200

    refactor(recipe_home_page): change ingredients saving in database

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,23 +1,18 @@
-import json
-
 from django import forms
-from django.forms import formset_factory
 
-from .models import Recipe
-
-
-class IngredientsForm(forms.Form):
-    quantity = forms.CharField(max_length=15)
-    name = forms.CharField(max_length=50)
+from .models import Ingredient, Recipe
 
 
-# IngredientFormSet = formset_factory(IngredientsForm)
+class IngredientsForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
 
 
 class CreateNewRecipe(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ["recipe_name", "ingredients", "preparation", "tags"]
+        fields = ["recipe_name", "preparation", "tags"]
 
 
 class RecipeIngredientsForm(forms.Form):
@@ -28,22 +23,7 @@ class RecipeIngredientsForm(forms.Form):
         recipe_name = self.data.get("recipe_name")
         preparation = self.data.get("preparation")
         tags = self.data.get("tags")
-        ingredients_list = list(
-            filter(lambda key: key.startswith("quantity-"), self.data.keys())
-        )
-        ingredients_json = {}
-        for prefix in ingredients_list:
-            quantity = self.data.get(prefix)
-            name = self.data.get(f"{prefix.replace('quantity', 'name')}")
-
-            ingredients_json[name] = quantity
-
-        recipe = Recipe(
-            recipe_name=recipe_name,
-            ingredients=json.dumps(ingredients_json, indent=4, ensure_ascii=False),
-            preparation=preparation,
-            tags=[tags],
-        )
+        recipe = Recipe(recipe_name=recipe_name, preparation=preparation, tags=[tags])
 
         if commit:
             recipe.save()
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
+import json
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
 from django.views import View
 
-from .forms import RecipeIngredientsForm
-from .models import Recipe
+from .forms import CreateNewRecipe, IngredientsForm, RecipeIngredientsForm
+from .models import Ingredient, Recipe
 
 
 def recipes_home_page(request):
-    user_recipes = Recipe.objects.all().filter(user_fk=request.user.pk)
+    user_recipes = Recipe.objects.all().filter(user=request.user.pk)
     context = {"title": "Recipes", "recipes": user_recipes}
 
     return render(request, "recipes/home.html", context)
@@ -19,8 +21,28 @@ class RecipesHomePageView(View):
     template_name = "recipes/home.html"
 
     def get(self, request):
-        user_recipes = Recipe.objects.all().filter(user_fk=request.user.pk)
-        context = {"title": "Recipes", "recipes": user_recipes}
+        user_recipes = Recipe.objects.all().filter(user=request.user.pk)
+        recipes_with_ingredients = []
+        for recipe in user_recipes:
+            recipe_data = {
+                "id": recipe.id,
+                "recipe_name": recipe.recipe_name,
+                "preparation": recipe.preparation,
+                "tags": recipe.tags,
+                "ingredients": [],
+            }
+
+            ingredients = recipe.ingredients.all()
+
+            for ingredient in ingredients:
+                ingredient_data = {
+                    "name": ingredient.name,
+                    "quantity": ingredient.quantity,
+                    "quantity_type": ingredient.quantity_type,
+                }
+                recipe_data["ingredients"].append(ingredient_data)
+            recipes_with_ingredients.append(recipe_data)
+        context = {"title": "Recipes", "recipes": recipes_with_ingredients}
 
         return render(request, self.template_name, context)
 
@@ -40,7 +62,37 @@ class RecipeAddPageView(View):
         form = RecipeIngredientsForm(request.POST)
         if form.is_valid():
             recipe = form.save(commit=False)
-            recipe.user_fk = request.user.profile
+            recipe.user = request.user.profile
             recipe.save()
+            self.add_ingredients_to_recipe(request.POST, recipe)
             messages.success(request, "Przepis został dodany")
             return redirect("recipes_home_page")
+
+    def add_ingredients_to_recipe(self, request, recipe):
+        ingredients_list = list(
+            filter(lambda key: key.startswith("quantity-"), request)
+        )
+        for prefix in ingredients_list:
+            quantity = request.get(prefix)
+            name = request.get(f"{prefix.replace('quantity', 'name')}")
+            quantity_type = request.get(
+                f"{prefix.replace('quantity', 'quantity_type')}"
+            )
+
+            ingredient = Ingredient.objects.get_or_create(
+                name=name, quantity=quantity, quantity_type=quantity_type
+            )
+            recipe.ingredients.add(ingredient[0].id)
+
+
+class RecipeEditPageView(View):
+    template_name = "recipes/recipe_form.html"
+
+    def get(self, request, recipe_id):
+        recipe = get_object_or_404(Recipe, pk=recipe_id)
+        ingredients = json.loads(recipe.ingredients)
+        form = CreateNewRecipe(instance=recipe)
+        ingredient_form = IngredientsForm(instance=ingredients)
+        context = {"form": form, "ingredient_form": ingredient_form}
+
+        return render(request, self.template_name, context)
