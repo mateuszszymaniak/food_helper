ab8994b32b90dc71b063e1915f17e082b2be6fba
Mateusz Szymaniak <maszipl@gmail.com>
2023-09-17 20:14:22 +0200

    feat(edit): add possibility to edit recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
 from . import views
-from .views import RecipeAddPageView, RecipesHomePageView
+from .views import RecipeAddPageView, RecipeEditPageView, RecipesHomePageView
 
 urlpatterns = [
     path("", RecipesHomePageView.as_view(), name="recipes_home_page"),
     path("add/", RecipeAddPageView.as_view(), name="recipes_add"),
+    path("edit/<int:recipe_id>/", RecipeEditPageView.as_view(), name="recipe_edit"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,13 @@ import json
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
 from django.views import View
 
 from .forms import CreateNewRecipe, IngredientsForm, RecipeIngredientsForm
-from .models import Ingredient, Recipe
+from .models import Ingredient, Recipe, RecipeIngredient
 
 
 def recipes_home_page(request):
@@ -21,7 +22,7 @@ class RecipesHomePageView(View):
     template_name = "recipes/home.html"
 
     def get(self, request):
-        user_recipes = Recipe.objects.all().filter(user=request.user.pk)
+        user_recipes = Recipe.objects.all().filter(user=request.user.pk).order_by("id")
         recipes_with_ingredients = []
         for recipe in user_recipes:
             recipe_data = {
@@ -90,9 +91,111 @@ class RecipeEditPageView(View):
 
     def get(self, request, recipe_id):
         recipe = get_object_or_404(Recipe, pk=recipe_id)
-        ingredients = json.loads(recipe.ingredients)
-        form = CreateNewRecipe(instance=recipe)
-        ingredient_form = IngredientsForm(instance=ingredients)
-        context = {"form": form, "ingredient_form": ingredient_form}
+        ingredients = recipe.ingredients.all()
+        context = {"form": recipe, "ingredient_form": ingredients}
 
         return render(request, self.template_name, context)
+
+    def post(self, request, recipe_id):
+        form = RecipeIngredientsForm(request.POST)
+        if form.is_valid():
+            recipe = Recipe.objects.get(id=recipe_id)
+            self.ingredients_change(recipe, form)
+            changes = self.find_changes(recipe, form)
+            if changes is not None:
+                for field, (old_value, new_value) in changes.items():
+                    setattr(recipe, field, new_value)
+            recipe.save()
+            messages.success(request, "Przepis został zaktualizowany")
+            return redirect("recipes_home_page")
+
+    def ingredients_change(self, recipe, form):
+        ingredients_list = list(
+            filter(lambda key: key.startswith("quantity-"), form.data)
+        )
+
+        old_ingredients = recipe.ingredients.all()
+        if len(old_ingredients) == len(ingredients_list):
+            for counter, prefix in enumerate(ingredients_list):
+                quantity = form.data.get(prefix)
+                name = form.data.get(f"{prefix.replace('quantity', 'name')}")
+                quantity_type = form.data.get(
+                    f"{prefix.replace('quantity', 'quantity_type')}"
+                )
+                try:
+                    ingredient = get_object_or_404(
+                        Ingredient,
+                        name=name,
+                        quantity=quantity,
+                        quantity_type=quantity_type,
+                    )
+                except Http404:
+                    ingredient = Ingredient.objects.create(
+                        name=name, quantity=quantity, quantity_type=quantity_type
+                    )
+
+                if ingredient != old_ingredients[counter]:
+                    recipe_ingredient = RecipeIngredient.objects.filter(
+                        recipe=recipe, ingredient=old_ingredients[counter]
+                    ).first()
+                    recipe_ingredient.ingredient = ingredient
+                    recipe_ingredient.save()
+        elif len(old_ingredients) < len(ingredients_list):
+            for prefix in ingredients_list:
+                quantity = form.data.get(prefix)
+                name = form.data.get(f"{prefix.replace('quantity', 'name')}")
+                quantity_type = form.data.get(
+                    f"{prefix.replace('quantity', 'quantity_type')}"
+                )
+                try:
+                    ingredient, created = Ingredient.objects.get_or_create(
+                        name=name, quantity=quantity, quantity_type=quantity_type
+                    )
+                    if not recipe.ingredients.filter(id=ingredient.id).exists():
+                        RecipeIngredient.objects.create(
+                            recipe=recipe, ingredient=ingredient
+                        )
+                except Exception as e:
+                    print(e)
+        else:
+            recipe_ingredient = RecipeIngredient.objects.filter(recipe=recipe)
+            actual_ingredients = []
+            for prefix in ingredients_list:
+                quantity = form.data.get(prefix)
+                name = form.data.get(f"{prefix.replace('quantity', 'name')}")
+                quantity_type = form.data.get(
+                    f"{prefix.replace('quantity', 'quantity_type')}"
+                )
+                try:
+                    ingredient, created = Ingredient.objects.get_or_create(
+                        name=name, quantity=quantity, quantity_type=quantity_type
+                    )
+                    actual_ingredients.append(ingredient.id)
+                    if not recipe.ingredients.filter(id=ingredient.id).exists():
+                        RecipeIngredient.objects.create(
+                            recipe=recipe, ingredient=ingredient
+                        )
+                except Exception as e:
+                    print(e)
+
+            delete_ingredients = recipe_ingredient.exclude(
+                ingredient_id__in=actual_ingredients
+            )
+            delete_ingredients.delete()
+
+    def find_changes(self, recipe, form):
+        changes = {}
+        recipe_name_from_recipe = recipe.recipe_name
+        recipe_name_from_form = form.data.get("recipe_name")
+        preparation_from_recipe = recipe.preparation
+        preparation_from_form = form.data.get("preparation")
+        tags_from_recipe = recipe.tags
+        tags_from_form = [form.data.get("tags")]
+
+        if recipe_name_from_recipe != recipe_name_from_form:
+            changes["recipe_name"] = (recipe_name_from_recipe, recipe_name_from_form)
+        if preparation_from_recipe != preparation_from_form:
+            changes["preparation"] = (preparation_from_recipe, preparation_from_form)
+        if tags_from_recipe != tags_from_form:
+            changes["tags"] = (tags_from_recipe, tags_from_form)
+        return changes
