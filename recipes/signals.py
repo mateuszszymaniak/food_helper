04495b2c333c1349a770f6d88cacc658ac01658a
Mateusz Szymaniak <maszipl@gmail.com>
2023-10-16 19:57:17 +0200

    fix(ingredients): add signal which delete ingredients attached with recipe

---
+++ recipes/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from recipes.models import Recipe
+
+
+@receiver(pre_delete, sender=Recipe)
+def pre_delete_recipe(sender, instance, **kwargs):
+    ingredients = instance.ingredients.all()
+
+    for ingredient in ingredients:
+        ingredient.delete()
