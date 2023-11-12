84abf47a0dca88e5d2d4286af1a8e77689587c7e
Mateusz Szymaniak <maszipl@gmail.com>
2023-11-12 21:19:09 +0100

    tests(fridge): add tests for fridge form

---
+++ fridges/tests/test_forms.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, tag
+
+from ingredients.factories import IngredientFactory
+
+from ..forms import NewIngredientForm
+
+
+@tag("x")
+class FridgeFormTest(TestCase):
+    def test_valid_fridge_form(self):
+        ingredient = IngredientFactory.create()
+        ingredient_data = {
+            "name": ingredient.name,
+            "quantity": ingredient.quantity,
+            "quantity_type": ingredient.quantity_type,
+        }
+        form = NewIngredientForm(data=ingredient_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_fridge_form(self):
+        ingredient_data = {}
+        form = NewIngredientForm(data=ingredient_data)
+        self.assertFalse(form.is_valid())
