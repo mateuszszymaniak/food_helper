f8792f49162d4e8be74fe7e52b2be69d46caef52
Mateusz Szymaniak <maszipl@gmail.com>
2024-01-11 22:45:53 +0100

    refactor(models): update models

--- recipe_ingredients/models.py
+++ recipe_ingredients/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from ingredients.models import Ingredient
@@ -5,7 +6,7 @@ from ingredients.models import Ingredient
 
 class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.DO_NOTHING)
-    amount = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f"{self.ingredient}, {self.amount}"
