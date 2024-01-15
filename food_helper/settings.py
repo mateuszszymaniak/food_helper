e523224d792427b21ff069a25d0cfcd23830b20b
Mateusz Szymaniak <maszipl@gmail.com>
2024-01-15 21:12:02 +0100

    feat: update env

--- food_helper/settings.py
+++ food_helper/settings.py
@@ -13,15 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import environ
-
 from .env import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
